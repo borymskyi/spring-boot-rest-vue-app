26361c1733f280b16840d1c25cf49b17bed89c0a
borymskyi <dimokskyn@gmail.com>
2022-05-29 18:33:19 +0300

    Add Vuex for storing state

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -13,53 +13,46 @@
           <a href="/login">Google</a>
         </v-container>
         <v-container v-if="profile">
-          <messages-list :messages="messages" />
+          <messages-list />
         </v-container>
       </v-main>
     </v-app>
 </template>
 
 <script>
-    import MessagesList from 'components/messages/MessageList.vue'
-    import { addHandler } from 'util/ws'
+  import { mapState, mapMutations } from 'vuex'
+  import MessagesList from 'components/messages/MessageList.vue'
+  import { addHandler } from 'util/ws'
 
-    export default {
-        components: {
-            MessagesList
-        },
-        data() {
-            return {
-                messages: frontendData.messages,
-                profile: frontendData.profile
-            }
-        },
-      created() {
-        addHandler(data => {
-          if (data.objectType === 'MESSAGE') {
-            const index = this.messages.findIndex(item => item.id === data.body.id)
-            switch (data.eventType) {
-              case 'CREATE':
-              case 'UPDATE':
-                if (index > -1) {
-                  this.messages.splice(index, 1, data.body)
-                } else {
-                  this.messages.push(data.body)
-                }
-                break
-              case 'REMOVE':
-                this.messages.splice(index, 1)
-                break
-              default:
-                console.error(`Looks like the event type if unknown "${data.eventType}"`)
-            }
-          } else {
-            console.error(`Looks like the object type if unknown "${data.objectType}"`)
-          }
-        })
+export default {
+  components: {
+    MessagesList
+  },
+  computed: mapState(['profile']),
+  methods: mapMutations(['addMessageMutation', 'updateMessageMutation', 'removeMessageMutation']),
+  created() {
+    addHandler(data => {
+      if (data.objectType === 'MESSAGE') {
+        switch (data.eventType) {
+          case 'CREATE':
+            this.addMessageMutation(data.body)
+            break
+          case 'UPDATE':
+            this.updateMessageMutation(data.body)
+            break
+          case 'REMOVE':
+            this.removeMessageMutation(data.body)
+            break
+          default:
+            console.error(`Looks like the event type if unknown "${data.eventType}"`)
+        }
+      } else {
+        console.error(`Looks like the object type if unknown "${data.objectType}"`)
       }
-    }
+    })
+  }
+}
 </script>
 
 <style>
-
 </style>
